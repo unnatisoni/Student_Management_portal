57d83d9679733189d0edb0dc11f123ed02d4be23
Your Name <you@example.com>
2021-07-25 00:15:49 +0530

    modified

--- src/Component/Button/Button.tsx
+++ src/Component/Button/Button.tsx
@@ -9,31 +9,31 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement>{
 }
 const Button: React.FC<Props> = ({children, buttonType,className, type, theme, ...rest}) => {
 
-let color="" , themedesign = " ";
+let color=' ' , themedesign = ' ';
 if(theme === 'primary'){
-  color = "blue"
+  color = 'blue';
 }
-else if (theme === "success"){
-  color = "green"
+else if (theme === 'success'){
+  color = 'green';
 }
-else if(theme === "Warning"){
-  color = "yellow"
+else if(theme === 'Warning'){
+  color = 'yellow';
 }
-else if(theme === "Danger"){
-  color = "red"
+else if(theme === 'Danger'){
+  color = 'red';
 }
-else if(theme === "Secondary"){
-  color = "indigo"
+else if(theme === 'Secondary'){
+  color = 'indigo';
 }
 else{
-  color="gray"
+  color='gray';
 }
 
-if ( buttonType === "solidify"){
-  themedesign = "bg-"+color+"-500" + " " + "text-white shadow-"+color + " "+ "hover:shadow-none";
+if ( buttonType === 'solidify'){
+  themedesign = 'bg-'+color+'-500' + ' ' + 'text-white shadow-'+color + ' '+'hover:shadow-none';
 }
 else{
-  themedesign = "bg-white text-"+color+"-700 border border-"+color+"-400 hover:shadow-"+color + " " + "hover:bg-"+color+"-700" + " "+ "hover:text-white"
+  themedesign = 'bg-white text-'+color+'-700 border border-'+color+'-400 hover:shadow-'+color + ' ' + 'hover:bg-'+color+'-700' + ' '+ 'hover:text-white';
 }
 
 
@@ -46,7 +46,7 @@ else{
             <button
                  {...rest}
                  
-                  className={" transition-shadow px-5 py-by text-base rounded-md disabled:cursor-not-allowed " + " " +  themedesign + " " +className}
+                  className={' transition-shadow px-5 py-by text-base rounded-md disabled:cursor-not-allowed ' + ' ' +  themedesign + ' ' +className}
                    >
                  {children}
                 </button>
